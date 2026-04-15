ef0f8078c34464d4c56daf292e0f9d33de55823c
abstudio01 <abstudio.ma@gmail.com>
2026-04-15 07:35:37 +0500

    Update Notification.js

--- models/Notification.js
+++ models/Notification.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
   userId:  { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  type:    { type: String, enum: ['new_task', 'revision', 'approved', 'cancelled'], required: true },
+  type:    { type: String, enum: ['new_task', 'revision', 'approved', 'cancelled', 'deadline_warning'], required: true },
   message: { type: String, required: true },
   taskId:  { type: mongoose.Schema.Types.ObjectId, ref: 'Task', default: null },
   read:    { type: Boolean, default: false },
